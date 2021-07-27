bb382425117a048d46bde9daae7d88b8637af7f2
himax82 <himax82@gmail.com>
2021-07-27 20:18:35 +0800

    5. Аннотация @Override [#174891 #182220]

--- src/main/java/ru/job4j/tracker/oop/profession/Builder.java
+++ src/main/java/ru/job4j/tracker/oop/profession/Builder.java
@@ -4,6 +4,10 @@ public class Builder extends Engineer {
 
     private String buildingType;
 
+    public Builder(String name, String surname, String educations, String birthday, String branch) {
+        super(name, surname, educations, birthday, branch);
+    }
+
     public Building building(Projects projects) {
         return new Building();
     }
