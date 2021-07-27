bfd319e2d88411189e995d26a2adf9374b9f2c60
himax82 <himax82@gmail.com>
2021-07-27 20:12:59 +0800

    5. Аннотация @Override [#174891 #182220]

--- src/main/java/ru/job4j/tracker/oop/profession/Surgeon.java
+++ src/main/java/ru/job4j/tracker/oop/profession/Surgeon.java
@@ -2,7 +2,13 @@ package ru.job4j.tracker.oop.profession;
 
 public class Surgeon extends Doctor {
 
-    public void operate(Pacients pacient, Diagnos diagnos) {
+    private String childOrAdult;
+
+    public Surgeon(String name, String surname, String educations, String birthday, String namesHospital) {
+        super(name, surname, educations, birthday, namesHospital);
+    }
+
+    public void operate(Pacients pacients, Diagnos diagnos) {
 
     }
 
