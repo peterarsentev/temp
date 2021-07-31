041f966d01f6832d07c4f235609795ac3efcca7a
himax82 <himax82@gmail.com>
2021-07-31 16:29:29 +0800

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+import java.util.SplittableRandom;
+
+public class FindEl {
+
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] == key) {
+                rsl = i;
+            } else {
+                throw new ElementNotFoundException("Элемент не найден!");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] str = {"I", "love", "java"};
+        String key = "C++";
+        try {
+            FindEl.indexOf(str, key);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
