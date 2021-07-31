30bf1195838608a7089b268fd446decc98d9e086
himax82 <himax82@gmail.com>
2021-07-31 16:04:40 +0800

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNumberNegative() {
+        new Fact().calc(-5);
+    }
+
+}
