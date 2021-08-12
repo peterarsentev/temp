05ba234d9e9f14efaead52b7792d10ab815f48be
job4j <himax82@gmail.com>
2021-08-12 15:24:06 +0800

    2.2. Лямбда блок [#249208 #188052]

---
+++ src/test/java/ru/job4j/encapsulation/OptionalOrElseThrowTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.encapsulation;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class OptionalOrElseThrowTest {
+
+    @Test
+    public void whenFound() {
+        OptionalOrElseThrow.User u1 = new OptionalOrElseThrow.User("u1");
+        OptionalOrElseThrow.User u2 = new OptionalOrElseThrow.User("u2");
+        OptionalOrElseThrow.User u3 = new OptionalOrElseThrow.User("u3");
+        assertEquals(u3.getLogin(), OptionalOrElseThrow.orElseThrow(
+                List.of(u1, u2, u3), u3.getLogin()
+        ).getLogin());
+    }
+
+    @Test(expected = OptionalOrElseThrow.UserNotFoundException.class)
+    public void whenNotFound() {
+        OptionalOrElseThrow.User u1 = new OptionalOrElseThrow.User("u1");
+        OptionalOrElseThrow.User u2 = new OptionalOrElseThrow.User("u2");
+        OptionalOrElseThrow.User u3 = new OptionalOrElseThrow.User("u3");
+        OptionalOrElseThrow.orElseThrow(List.of(u1, u2, u3), "u4");
+    }
+
+}
