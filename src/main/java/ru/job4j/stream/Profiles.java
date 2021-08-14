b9319731c87ead62d4f3ce990ea6552cf11c4eb6
himax82 <himax82@gmail.com>
2021-08-14 21:13:38 +0800

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,11 +1,17 @@
 package ru.job4j.stream;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
 
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        List<Address> addressList = (List<Address>) profiles.stream().map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
+                .collect(Collectors.toList());
+        return addressList;
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -22,4 +22,29 @@ public class ProfilesTest {
         assertThat(stAddress.get(1).getStreet(), is("Lenina"));
     }
 
+    @Test
+    public void whenSortDublicat() {
+    List<Profile> students = List.of(
+            new Profile(new Address("Ulan", "Gagarina", 45, 3)),
+            new Profile (new Address("Moscow", "Lenina", 5, 1)),
+            new Profile (new Address("Moscow", "Lenina", 5, 1))
+    );
+    Profiles profiles = new Profiles();
+    List<Address> stAddress = profiles.collect(students);
+    assertThat(stAddress.size(), is(2));
+}
+
+    @Test
+    public void whenSortCollect() {
+        List<Profile> students = List.of(
+                new Profile(new Address("Ulan", "Gagarina", 45, 3)),
+                new Profile (new Address("Moscow", "Lenina", 5, 1)),
+                new Profile (new Address("Moscow", "Lenina", 5, 1))
+        );
+        Profiles profiles = new Profiles();
+        List<Address> stAddress = profiles.collect(students);
+        assertThat(stAddress.get(0).getCity(), is("Moscow"));
+    }
+
+
 }
